b3c66f18d60cf449deadb2b6126a1f3926e4a7d3
ineffable-coder-0435 <gerren62@gmail.com>
2020-04-21 17:13:27 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -18,10 +18,10 @@ function sayHiToGrandma(string) {
   if (string.toLowerCase()) {
     return "I can\'t hear you!";
 
-else if (string.toUpperCase())
+  else if (string.toUpperCase())
     return "YES INDEED!";
 
-else (string === "I love you, Grandma.")
+  else (string === "I love you, Grandma.")
     return "I love you, too.";
     }
   }
